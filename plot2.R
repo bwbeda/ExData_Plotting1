0cccf5fb0cdef6f931b43ad53c0936b8e1ac3bcf
Brent Benson <bwbensonjr@gmail.com>
2014-05-06 14:48:36 -0400

    Added missing line to code for second plot.

--- plot2.R
+++ plot2.R
@@ -12,6 +12,9 @@ power$Date_Obj <- as.Date(power$Date, "%d/%m/%Y")
 # Select the two day sample before doing other transformations and graphing
 power2d <- power[power$Date_Obj == "2007-02-01" | power$Date_Obj == "2007-02-02",]
 
+# Create a DateTime column that orders all of the readings for the two day sample
+power2d$Date_Time <- strptime(paste(power2d$Date, power2d$Time), "%d/%m/%Y %H:%M:%S")
+
 # Generate Plot 2
 png("plot2.png", width=480, height=480, bg="transparent")
 with(power2d, {
